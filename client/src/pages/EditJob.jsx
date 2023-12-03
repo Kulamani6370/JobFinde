a2e96eab833487cc22da5841c6b514de0405b228
Kulamani6370 <130361066+Kulamani6370@users.noreply.github.com>
2023-12-03 17:55:12 +0530

    Updated EditJob page

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -4,7 +4,7 @@ import { useLoaderData, useParams } from "react-router-dom";
 import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
 import { Form, useNavigation, redirect } from "react-router-dom";
 import { toast } from "react-toastify";
-import customFetch from "../utils/customFetch";
+import customFetch from "../Utils/customFetch";
 
 export const loader = async ({ params }) => {
   try {
